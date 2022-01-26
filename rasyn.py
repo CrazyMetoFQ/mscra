05b02678c8d7c5d249e84417dd109af289351650
CrazyMetoFQ <74641067+CrazyMetoFQ@users.noreply.github.com>
2022-01-26 14:56:55 +0400

    runs asyn in ipython
    runs the asynchronous stuff in ipython notebook,
    through the command line.
    
    use ! in jupyter for cli

---
+++ rasyn.py
@@ -0,0 +1,76 @@
+"""
+runs the asyncronous lib
+throught the cli
+
+works in ipython through this work around
+"""
+
+import asyn as ayn
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("base_link")
+parser.add_argument("min_")
+parser.add_argument("max_")
+parser.add_argument("single")
+
+parser.add_argument("base_path")
+
+parser.add_argument("save_As")
+parser.add_argument("file_Format")
+
+parser.add_argument("do_print")
+parser.add_argument("round_to")
+
+parser.add_argument("img_args")
+parser.add_argument("debug__")
+parser.add_argument("ikey")
+
+args = parser.parse_args()
+
+if args.do_print == "True":
+  print(1 ,": " ,args, '\n')
+else: pass
+
+#changing str to none or int
+imar = args.img_args.split(',')
+for  sno,ima in enumerate(imar):
+  if ima == "None":
+    imar[sno] = None
+  else: 
+    imar[sno] = int(ima)
+
+
+if args.debug__ == "False":
+  args.debug__ = False
+elif args.debug__ == "True":
+  args.debug__ = True
+else:
+  print("eror")
+
+# base_link where the number will change in the link such as ch-1, ch-2 put {}
+base_link = args.base_link
+# putting numbers in the link
+if args.single != "True":
+  pg_links = [base_link.format(i) for i in range(int(args.min_),int(args.max_))]
+else:
+  pg_links = [base_link]
+
+# path to where the pdfs will reside
+ch_path = args.base_path
+
+# # base_link where the number will change in the link such as ch-1, ch-2 put {}
+# base_link = "https://www.readdemonslayerarc.com/demon-slayer-chapter-{}/"
+
+
+
+# MAIN FUNCTION
+ayn.main_func(pg_links, 
+              ch_path, 
+              (args.save_As, args.file_Format),
+              int(args.round_to),
+              args.debug__,
+              args.ikey,
+              imgs_kwargs={'strt':imar[0],'stp':imar[1],'step':imar[2] }
+              )
