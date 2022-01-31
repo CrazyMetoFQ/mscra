a70af6d4251dfd5d5ff2d2c3b59f3fe77bdbb1b7
CrazyMetoFQ <74641067+CrazyMetoFQ@users.noreply.github.com>
2022-01-31 16:45:38 +0400

    Update rasyn.py

--- rasyn.py
+++ rasyn.py
@@ -1,3 +1,4 @@
+
 """
 runs the asyncronous lib
 throught the cli
@@ -13,7 +14,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument("base_link")
 parser.add_argument("min_")
 parser.add_argument("max_")
+
 parser.add_argument("single")
+parser.add_argument("home_link")
+parser.add_argument("raw_csv")
+
 
 parser.add_argument("base_path")
 
@@ -26,7 +31,7 @@ parser.add_argument("round_to")
 parser.add_argument("img_args")
 parser.add_argument("debug__")
 parser.add_argument("ikey")
-parser.add_argument("home_link")
+
 
 args = parser.parse_args()
 
@@ -50,13 +55,31 @@ elif args.debug__ == "True":
 else:
   print("eror")
 
+
+if args.raw_csv == "False":
+  args.raw_csv = False
+elif args.raw_csv == "True":
+  args.raw_csv = True
+else:
+  print("eror")
+  
+
 # base_link where the number will change in the link such as ch-1, ch-2 put {}
 base_link = args.base_link
 # putting numbers in the link
-if args.single != "True":
+if args.single != "True" and not args.raw_csv:
   pg_links = [base_link.format(i) for i in range(int(args.min_),int(args.max_))]
-else:
-  pg_links = [base_link]
+
+else:  
+  if args.raw_csv:
+    with open("mscra/pg_links_.csv") as f:
+      rcsv = f.read()
+      
+    pg_links = [s.replace(" ", "") for s in rcsv.split(",")]
+   
+  else: 
+      pg_links = [base_link]
+
 
 # path to where the pdfs will reside
 ch_path = args.base_path
@@ -76,3 +99,6 @@ ayn.main_func(pg_links,
               args.ikey,
               imgs_kwargs={'strt':imar[0],'stp':imar[1],'step':imar[2] }
               )
+
+
+
